6505b09202f616fb2cd65ce7d6ec101ed6f398b9
Kevin <mr.kev@me.com>
2015-02-17 03:02:46 -0500

    Apparently 0.0.0.0 is for the local network lol

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 module.exports = {
-  ipaddr: process.env.OPENSHIFT_NODEJS_IP || "127.0.0.1",
+  ipaddr: process.env.OPENSHIFT_NODEJS_IP || "0.0.0.0",
   port: process.env.OPENSHIFT_NODEJS_PORT || 3000
 }
